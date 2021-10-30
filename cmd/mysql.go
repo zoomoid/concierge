f61a76b78bc367fac00ac2b844f4a1824e5cf7de
Alexander Bartolomey <occloxium@gmail.com>
2021-10-30 12:48:34 +0200

    feat: draft mysql command
    Signed-off-by: Alexander Bartolomey <occloxium@gmail.com>

--- cmd/mysql.go
+++ cmd/mysql.go
@@ -1,1 +1,22 @@
 package cmd
+
+import "github.com/spf13/cobra"
+
+var (
+	MysqlDatabase string
+	MysqlPassword string
+	MysqlUsername string
+	mysqlCmd      = &cobra.Command{
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return nil
+		},
+	}
+)
+
+func init() {
+	rootCmd.AddCommand(mysqlCmd)
+
+	mysqlCmd.Flags().StringVarP(&MysqlDatabase, "database", "d", "", "Database name to create")
+	mysqlCmd.Flags().StringVarP(&MysqlUsername, "username", "u", "", "Username of user to create")
+	mysqlCmd.Flags().StringVarP(&MysqlPassword, "password", "p", "", "Password of user to create")
+}
