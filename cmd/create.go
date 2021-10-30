b1a8174f3a71fa105097ae722d01806dfdf6ea20
Alexander Bartolomey <occloxium@gmail.com>
2021-10-30 12:48:27 +0200

    refactor: generalize connection parameters
    Signed-off-by: Alexander Bartolomey <occloxium@gmail.com>

--- cmd/create.go
+++ cmd/create.go
@@ -6,12 +6,21 @@ import (
 	"github.com/spf13/cobra"
 )
 
-func init() {
-	rootCmd.AddCommand(createCmd)
+type DatabaseConnection struct {
+	Url      string
+	Host     string
+	Port     string
+	Username string
+	Password string
 }
 
 var (
-	createCmd = &cobra.Command{
+	Url                string
+	Host               string
+	Port               string
+	ConnectionUsername string
+	ConnectionPassword string
+	createCmd          = &cobra.Command{
 		Use:   "create",
 		Short: "Creates a new database",
 		Long:  `Templates a new database from a given type, name, user, and password`,
@@ -20,3 +29,13 @@ var (
 		},
 	}
 )
+
+func init() {
+	rootCmd.AddCommand(createCmd)
+
+	createCmd.Flags().StringVarP(&Url, "url", "C", "", "A fully-qualified connection URL to the database")
+	createCmd.Flags().StringVarP(&Host, "host", "H", "", "The database hostname to connect to")
+	createCmd.Flags().StringVarP(&Port, "port", "P", "", "The database port at which to connect to")
+	createCmd.Flags().StringVarP(&ConnectionUsername, "username", "U", "", "Database user to connect with")
+	createCmd.Flags().StringVarP(&ConnectionPassword, "password", "A", "", "Database password to connect with")
+}
